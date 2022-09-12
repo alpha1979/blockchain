2099be616e64f32a58f6e2a26bdbdb252a46ee25
alpha1979 <atit1979@hotmail.com>
2022-09-12 11:39:50 +0100

    First commit for blockchain app

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const app = express();
+
+const Block = require('./block');
+const Blockchain = require('./blockchain');
+const Transaction = require('./transaction');
+
+app.use(express.json());
+
+let transactions = [];
+
+let genesisBlock = new Block();
+
+let blockchain = new Blockchain(genesisBlock);
+
+app.post('/transactions', (req, res) => {
+    const to = req.body.to;
+    const from = req.body.from;
+    const amount = req.body.amount;
+    let transaction = new Transaction(from, to, amount);
+    transactions.push(transaction);
+    res.json(transactions);
+});
+
+app.get('/blockchain', (req, res) => {
+    res.json(blockchain);
+});
+
+app.get('/mine', (req, res) => {
+    let block = blockchain.getNextBlock(transactions);
+    blockchain.addBlock(block);
+    res.json(block);
+})
+
+app.listen(8080, () => {
+    console.log('Server is running');
+});
